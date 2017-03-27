019e7ff43367989030ec5f2ad3106d7bdb083c32
Gaëtan Angely <gangely@users.noreply.github.com>
2017-03-27 18:22:49 +0200

    Update tmp36.fs

--- jnz/tmp36.fs
+++ jnz/tmp36.fs
@@ -1,7 +1,22 @@
+\ tmp36.fs
+\ gea 20170325
+
+\ needs adc (already loaded in jnz)
+
 : tmp36 ( -- n1 ) pa0 adc ;
+
 : adc>volt ( n1 -- n2 )  3300 4095 */ ;
+
 : adc>temp ( n1 -- n2 )  adc>volt 500 - ;  \ in tenths of °C \
+
 : #.#s ( d -- n addr ) <# # 46 hold #s #> ;
+
 : .temp ( n1 -- ) adc>temp s>d #.#s type ;
+
 : show-temp ( -- ) tmp36 ." temp: " .temp ." °C" ;
+
 : show-tmp36 ( -- ) tmp36 ." TMP36 sample: " dup . ." -- voltage: " dup adc>volt . ." mV" ."  -- temp: " .temp ." °C" ;
+
+\ init-adc
+\ show-temp
+\ show-tmp36
